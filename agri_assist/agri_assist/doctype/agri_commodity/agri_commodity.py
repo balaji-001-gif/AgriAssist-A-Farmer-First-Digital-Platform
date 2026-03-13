c101405634161cc48b4f262cdbd7a4202accbe77
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-03-13 16:29:09 +0530

    Fix: Add Python module files for all Doctypes

---
+++ agri_assist/agri_assist/doctype/agri_commodity/agri_commodity.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class AgriCommodity(Document):
+    pass
---
+++ agri_assist/agri_assist/doctype/agri_knowledge_article/agri_knowledge_article.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class AgriKnowledgeArticle(Document):
+    pass
